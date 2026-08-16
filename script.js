4aa517a8a43a223825c2d63a9b6dfc2b1db75ebc
Mohammed Al Otaibi <156099784+OneiMoment@users.noreply.github.com>
2026-08-16 05:24:20 +0300

    update

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 /**
  * Dynamic CV Application Script
  * Developer: Mohammed Al Otaibi
- * Version: 2.5 (Clean, Multilingual, Standard ISO 18004 QR, PWA, Back-to-top)
+ * Version: 2.6 (Bilingual UI, Standard QR Generator, vCard, WebShare, PWA)
  */
 
 // Application State
@@ -88,385 +88,13 @@ function initDOMCache() {
   DOM.qrDownloadText = document.getElementById('qrDownloadText');
   DOM.qrCopyLinkText = document.getElementById('qrCopyLinkText');
   DOM.qrModalCloseBtn = document.getElementById('qrModalCloseBtn');
-  DOM.qrCanvas = document.getElementById('qrCanvas');
+  DOM.qrcodeContainer = document.getElementById('qrcodeContainer');
   
   // Toast & Back-to-top
   DOM.toastNotification = document.getElementById('toastNotification');
   DOM.toastMessage = document.getElementById('toastMessage');
   DOM.backToTopBtn = document.getElementById('backToTopBtn');
 }
-
-/**
- * Standard ISO/IEC 18004 QR Code Generator Engine (Zero External Dependencies)
- */
-const QRGen = (function() {
-  function QRCode(typeNumber, errorCorrectionLevel) {
-    this.typeNumber = typeNumber;
-    this.errorCorrectionLevel = errorCorrectionLevel;
-    this.modules = null;
-    this.moduleCount = 0;
-    this.dataCache = null;
-    this.dataList = [];
-  }
-
-  const QRMode = { MODE_8BIT_BYTE: 1 << 2 };
-  const QRMaskPattern = {
-    PATTERN000: 0, PATTERN001: 1, PATTERN010: 2, PATTERN011: 3,
-    PATTERN100: 4, PATTERN101: 5, PATTERN110: 6, PATTERN111: 7
-  };
-
-  const QRUtil = {
-    PATTERN_POSITION_TABLE: [
-      [], [6, 18], [6, 22], [6, 26], [6, 30], [6, 34],
-      [6, 22, 38], [6, 24, 42], [6, 26, 46], [6, 28, 50]
-    ],
-    G15: (1 << 10) | (1 << 8) | (1 << 5) | (1 << 4) | (1 << 2) | (1 << 1) | (1 << 0),
-    G15_MASK: (1 << 14) | (1 << 12) | (1 << 10) | (1 << 4) | (1 << 1),
-    getBCHTypeInfo: function(data) {
-      let d = data << 10;
-      while (QRUtil.getBCHDigit(d) - QRUtil.getBCHDigit(QRUtil.G15) >= 0) {
-        d ^= QRUtil.G15 << (QRUtil.getBCHDigit(d) - QRUtil.getBCHDigit(QRUtil.G15));
-      }
-      return ((data << 10) | d) ^ QRUtil.G15_MASK;
-    },
-    getBCHDigit: function(data) {
-      let digit = 0;
-      while (data != 0) {
-        digit++;
-        data >>>= 1;
-      }
-      return digit;
-    },
-    getPatternPosition: function(typeNumber) {
-      return QRUtil.PATTERN_POSITION_TABLE[typeNumber - 1];
-    },
-    getMask: function(maskPattern, i, j) {
-      switch (maskPattern) {
-        case QRMaskPattern.PATTERN000: return (i + j) % 2 == 0;
-        case QRMaskPattern.PATTERN001: return i % 2 == 0;
-        case QRMaskPattern.PATTERN010: return j % 3 == 0;
-        case QRMaskPattern.PATTERN011: return (i + j) % 3 == 0;
-        case QRMaskPattern.PATTERN100: return (Math.floor(i / 2) + Math.floor(j / 3)) % 2 == 0;
-        case QRMaskPattern.PATTERN101: return ((i * j) % 2) + ((i * j) % 3) == 0;
-        case QRMaskPattern.PATTERN110: return (((i * j) % 2) + ((i * j) % 3)) % 2 == 0;
-        case QRMaskPattern.PATTERN111: return (((i * j) % 3) + ((i + j) % 2)) % 2 == 0;
-        default: throw new Error("bad maskPattern:" + maskPattern);
-      }
-    },
-    getErrorCorrectPolynomial: function(errorCorrectLength) {
-      let a = new QRPolynomial([1], 0);
-      for (let i = 0; i < errorCorrectLength; i++) {
-        a = a.multiply(new QRPolynomial([1, QRMath.gexp(i)], 0));
-      }
-      return a;
-    },
-    getLengthInBits: function(mode, type) {
-      return (1 <= type && type < 10) ? 8 : 16;
-    }
-  };
-
-  const QRMath = {
-    glog: function(n) {
-      if (n < 1) throw new Error("glog(" + n + ")");
-      return QRMath.LOG_TABLE[n];
-    },
-    gexp: function(n) {
-      while (n < 0) n += 255;
-      while (n >= 255) n -= 255;
-      return QRMath.EXP_TABLE[n];
-    },
-    EXP_TABLE: new Array(256),
-    LOG_TABLE: new Array(256)
-  };
-
-  for (let i = 0; i < 8; i++) QRMath.EXP_TABLE[i] = 1 << i;
-  for (let i = 8; i < 256; i++) {
-    QRMath.EXP_TABLE[i] =
-      QRMath.EXP_TABLE[i - 4] ^ QRMath.EXP_TABLE[i - 5] ^
-      QRMath.EXP_TABLE[i - 6] ^ QRMath.EXP_TABLE[i - 8];
-  }
-  for (let i = 0; i < 255; i++) QRMath.LOG_TABLE[QRMath.EXP_TABLE[i]] = i;
-
-  function QRPolynomial(num, shift) {
-    if (num.length == undefined) throw new Error(num.length + "/" + shift);
-    let offset = 0;
-    while (offset < num.length && num[offset] == 0) offset++;
-    this.num = new Array(num.length - offset + shift);
-    for (let i = 0; i < num.length - offset; i++) this.num[i] = num[i + offset];
-    for (let i = num.length - offset; i < this.num.length; i++) this.num[i] = 0;
-  }
-
-  QRPolynomial.prototype = {
-    get: function(index) { return this.num[index]; },
-    getLength: function() { return this.num.length; },
-    multiply: function(e) {
-      const num = new Array(this.getLength() + e.getLength() - 1).fill(0);
-      for (let i = 0; i < this.getLength(); i++) {
-        for (let j = 0; j < e.getLength(); j++) {
-          num[i + j] ^= QRMath.gexp(QRMath.glog(this.get(i)) + QRMath.glog(e.get(j)));
-        }
-      }
-      return new QRPolynomial(num, 0);
-    },
-    mod: function(e) {
-      if (this.getLength() - e.getLength() < 0) return this;
-      const ratio = QRMath.glog(this.get(0)) - QRMath.glog(e.get(0));
-      const num = new Array(this.getLength());
-      for (let i = 0; i < this.getLength(); i++) num[i] = this.get(i);
-      for (let i = 0; i < e.getLength(); i++) {
-        num[i] ^= QRMath.gexp(QRMath.glog(e.get(i)) + ratio);
-      }
-      return new QRPolynomial(num, 0).mod(e);
-    }
-  };
-
-  const QRRSBlock = {
-    RS_BLOCK_TABLE: [
-      [1, 26, 19], [1, 26, 16], [1, 26, 13], [1, 26, 9],
-      [1, 44, 34], [1, 44, 28], [1, 44, 22], [1, 44, 16],
-      [1, 70, 55], [1, 70, 44], [2, 35, 17], [2, 35, 13],
-      [1, 100, 80], [2, 50, 32], [2, 50, 24], [4, 25, 9],
-      [1, 134, 108], [2, 67, 43], [2, 33, 15, 2, 34, 16], [2, 33, 11, 2, 34, 12],
-      [2, 86, 68], [4, 43, 27], [4, 43, 19], [4, 43, 15]
-    ],
-    getRSBlocks: function(typeNumber, errorCorrectionLevel) {
-      const rsBlock = QRRSBlock.RS_BLOCK_TABLE[(typeNumber - 1) * 4 + errorCorrectionLevel];
-      const length = rsBlock.length / 3;
-      const list = [];
-      for (let i = 0; i < length; i++) {
-        const count = rsBlock[i * 3 + 0];
-        const totalCount = rsBlock[i * 3 + 1];
-        const dataCount = rsBlock[i * 3 + 2];
-        for (let j = 0; j < count; j++) list.push({ totalCount, dataCount });
-      }
-      return list;
-    }
-  };
-
-  function QRBitBuffer() {
-    this.buffer = [];
-    this.length = 0;
-  }
-  QRBitBuffer.prototype = {
-    get: function(index) {
-      return ((this.buffer[Math.floor(index / 8)] >>> (7 - (index % 8))) & 1) == 1;
-    },
-    put: function(num, length) {
-      for (let i = 0; i < length; i++) this.putBit(((num >>> (length - i - 1)) & 1) == 1);
-    },
-    getLengthInBits: function() { return this.length; },
-    putBit: function(bit) {
-      const bufIndex = Math.floor(this.length / 8);
-      if (this.buffer.length <= bufIndex) this.buffer.push(0);
-      if (bit) this.buffer[bufIndex] |= 0x80 >>> (this.length % 8);
-      this.length++;
-    }
-  };
-
-  QRCode.prototype = {
-    addData: function(data) {
-      this.dataList.push({
-        mode: QRMode.MODE_8BIT_BYTE,
-        data: data,
-        getLength: function() { return data.length; },
-        write: function(buffer) {
-          for (let i = 0; i < data.length; i++) buffer.put(data.charCodeAt(i), 8);
-        }
-      });
-      this.dataCache = null;
-    },
-    isDark: function(row, col) { return this.modules[row][col]; },
-    getModuleCount: function() { return this.moduleCount; },
-    make: function() {
-      let typeNumber = 1;
-      for (typeNumber = 1; typeNumber < 10; typeNumber++) {
-        const rsBlocks = QRRSBlock.getRSBlocks(typeNumber, 0); // Level M
-        const buffer = new QRBitBuffer();
-        let totalDataCount = 0;
-        for (let i = 0; i < rsBlocks.length; i++) totalDataCount += rsBlocks[i].dataCount;
-        for (let i = 0; i < this.dataList.length; i++) {
-          const d = this.dataList[i];
-          buffer.put(d.mode, 4);
-          buffer.put(d.getLength(), QRUtil.getLengthInBits(d.mode, typeNumber));
-          d.write(buffer);
-        }
-        if (buffer.getLengthInBits() <= totalDataCount * 8) break;
-      }
-      this.typeNumber = typeNumber;
-      this.moduleCount = this.typeNumber * 4 + 17;
-      this.modules = new Array(this.moduleCount);
-      for (let row = 0; row < this.moduleCount; row++) {
-        this.modules[row] = new Array(this.moduleCount).fill(null);
-      }
-      this.setupPositionProbePattern(0, 0);
-      this.setupPositionProbePattern(this.moduleCount - 7, 0);
-      this.setupPositionProbePattern(0, this.moduleCount - 7);
-      this.setupPositionAdjustPattern();
-      this.setupTimingPattern();
-      this.setupTypeInfo(false, QRMaskPattern.PATTERN000);
-      this.dataCache = QRCode.createData(this.typeNumber, 0, this.dataList);
-      this.mapData(this.dataCache, QRMaskPattern.PATTERN000);
-    },
-    setupPositionProbePattern: function(row, col) {
-      for (let r = -1; r <= 7; r++) {
-        if (row + r <= -1 || this.moduleCount <= row + r) continue;
-        for (let c = -1; c <= 7; c++) {
-          if (col + c <= -1 || this.moduleCount <= col + c) continue;
-          if ((0 <= r && r <= 6 && (c == 0 || c == 6)) ||
-              (0 <= c && c <= 6 && (r == 0 || r == 6)) ||
-              (2 <= r && r <= 4 && 2 <= c && c <= 4)) {
-            this.modules[row + r][col + c] = true;
-          } else {
-            this.modules[row + r][col + c] = false;
-          }
-        }
-      }
-    },
-    setupTimingPattern: function() {
-      for (let r = 8; r < this.moduleCount - 8; r++) {
-        if (this.modules[r][6] == null) this.modules[r][6] = r % 2 == 0;
-      }
-      for (let c = 8; c < this.moduleCount - 8; c++) {
-        if (this.modules[6][c] == null) this.modules[6][c] = c % 2 == 0;
-      }
-    },
-    setupPositionAdjustPattern: function() {
-      const pos = QRUtil.getPatternPosition(this.typeNumber);
-      if (!pos) return;
-      for (let i = 0; i < pos.length; i++) {
-        for (let j = 0; j < pos.length; j++) {
-          const row = pos[i];
-          const col = pos[j];
-          if (this.modules[row][col] != null) continue;
-          for (let r = -2; r <= 2; r++) {
-            for (let c = -2; c <= 2; c++) {
-              this.modules[row + r][col + c] = (r == -2 || r == 2 || c == -2 || c == 2 || (r == 0 && c == 0));
-            }
-          }
-        }
-      }
-    },
-    setupTypeInfo: function(test, maskPattern) {
-      const data = (0 << 3) | maskPattern; // Level M
-      const bits = QRUtil.getBCHTypeInfo(data);
-      for (let i = 0; i < 15; i++) {
-        const mod = !test && ((bits >> i) & 1) == 1;
-        if (i < 6) this.modules[i][8] = mod;
-        else if (i < 8) this.modules[i + 1][8] = mod;
-        else this.modules[this.moduleCount - 15 + i][8] = mod;
-
-        if (i < 8) this.modules[8][this.moduleCount - i - 1] = mod;
-        else if (i < 9) this.modules[8][15 - i - 1 + 1] = mod;
-        else this.modules[8][15 - i - 1] = mod;
-      }
-      this.modules[this.moduleCount - 8][8] = !test;
-    },
-    mapData: function(data, maskPattern) {
-      let inc = -1;
-      let row = this.moduleCount - 1;
-      let bitIndex = 7;
-      let byteIndex = 0;
-      for (let col = this.moduleCount - 1; col > 0; col -= 2) {
-        if (col == 6) col--;
-        while (true) {
-          for (let c = 0; c < 2; c++) {
-            if (this.modules[row][col - c] == null) {
-              let dark = false;
-              if (byteIndex < data.length) dark = ((data[byteIndex] >>> bitIndex) & 1) == 1;
-              const mask = QRUtil.getMask(maskPattern, row, col - c);
-              if (mask) dark = !dark;
-              this.modules[row][col - c] = dark;
-              bitIndex--;
-              if (bitIndex == -1) {
-                byteIndex++;
-                bitIndex = 7;
-              }
-            }
-          }
-          row += inc;
-          if (row < 0 || this.moduleCount <= row) {
-            row -= inc;
-            inc = -inc;
-            break;
-          }
-        }
-      }
-    }
-  };
-
-  QRCode.createData = function(typeNumber, errorCorrectionLevel, dataList) {
-    const rsBlocks = QRRSBlock.getRSBlocks(typeNumber, errorCorrectionLevel);
-    const buffer = new QRBitBuffer();
-    for (let i = 0; i < dataList.length; i++) {
-      const data = dataList[i];
-      buffer.put(data.mode, 4);
-      buffer.put(data.getLength(), QRUtil.getLengthInBits(data.mode, typeNumber));
-      data.write(buffer);
-    }
-    let totalDataCount = 0;
-    for (let i = 0; i < rsBlocks.length; i++) totalDataCount += rsBlocks[i].dataCount;
-    if (buffer.getLengthInBits() + 4 <= totalDataCount * 8) buffer.put(0, 4);
-    while (buffer.getLengthInBits() % 8 != 0) buffer.putBit(false);
-    while (true) {
-      if (buffer.getLengthInBits() >= totalDataCount * 8) break;
-      buffer.put(0xec, 8);
-      if (buffer.getLengthInBits() >= totalDataCount * 8) break;
-      buffer.put(0x11, 8);
-    }
-    return QRCode.createBytes(buffer, rsBlocks);
-  };
-
-  QRCode.createBytes = function(buffer, rsBlocks) {
-    let offset = 0;
-    let maxDcCount = 0;
-    let maxEcCount = 0;
-    const dcdata = new Array(rsBlocks.length);
-    const ecdata = new Array(rsBlocks.length);
-    for (let r = 0; r < rsBlocks.length; r++) {
-      const dcCount = rsBlocks[r].dataCount;
-      const ecCount = rsBlocks[r].totalCount - dcCount;
-      maxDcCount = Math.max(maxDcCount, dcCount);
-      maxEcCount = Math.max(maxEcCount, ecCount);
-      dcdata[r] = new Array(dcCount);
-      for (let i = 0; i < dcdata[r].length; i++) {
-        dcdata[r][i] = 0xff & buffer.buffer[i + offset];
-      }
-      offset += dcCount;
-      const rsPoly = QRUtil.getErrorCorrectPolynomial(ecCount);
-      const rawPoly = new QRPolynomial(dcdata[r], rsPoly.getLength() - 1);
-      const modPoly = rawPoly.mod(rsPoly);
-      ecdata[r] = new Array(rsPoly.getLength() - 1);
-      for (let i = 0; i < ecdata[r].length; i++) {
-        const modIndex = i + modPoly.getLength() - ecdata[r].length;
-        ecdata[r][i] = modIndex >= 0 ? modPoly.get(modIndex) : 0;
-      }
-    }
-    let totalCodeCount = 0;
-    for (let i = 0; i < rsBlocks.length; i++) totalCodeCount += rsBlocks[i].totalCount;
-    const data = new Array(totalCodeCount);
-    let index = 0;
-    for (let i = 0; i < maxDcCount; i++) {
-      for (let r = 0; r < rsBlocks.length; r++) {
-        if (i < dcdata[r].length) data[index++] = dcdata[r][i];
-      }
-    }
-    for (let i = 0; i < maxEcCount; i++) {
-      for (let r = 0; r < rsBlocks.length; r++) {
-        if (i < ecdata[r].length) data[index++] = ecdata[r][i];
-      }
-    }
-    return data;
-  };
-
-  return {
-    generate: function(text) {
-      const qr = new QRCode(0, 0);
-      qr.addData(text);
-      qr.make();
-      return qr;
-    }
-  };
-})();
 
 /**
  * Initialize Application Settings (Theme & Language)
@@ -1061,33 +689,31 @@ function copyProfileLink() {
 }
 
 /**
- * Render QR Code on Canvas using Standard ISO 18004 Generator
+ * Generate QR Code into Container (100% Scannable URL)
  */
-function drawQRCode(canvas, urlText) {
-  const ctx = canvas.getContext('2d');
-  const qr = QRGen.generate(urlText);
-  const count = qr.getModuleCount();
-  const canvasSize = canvas.width;
-  const padding = 16;
-  const cellSize = (canvasSize - padding * 2) / count;
+let qrInstance = null;
+function generateWebsiteQRCode() {
+  const targetUrl = 'https://cv.mohammed.alotaibi.site/';
+  if (!DOM.qrcodeContainer) return;
+  DOM.qrcodeContainer.innerHTML = '';
 
-  // Clear & fill white background
-  ctx.fillStyle = '#ffffff';
-  ctx.fillRect(0, 0, canvasSize, canvasSize);
-
-  // Draw dark modules
-  ctx.fillStyle = '#090d16';
-  for (let r = 0; r < count; r++) {
-    for (let c = 0; c < count; c++) {
-      if (qr.isDark(r, c)) {
-        ctx.fillRect(
-          Math.round(padding + c * cellSize),
-          Math.round(padding + r * cellSize),
-          Math.ceil(cellSize),
-          Math.ceil(cellSize)
-        );
-      }
-    }
+  if (typeof QRCode !== 'undefined') {
+    qrInstance = new QRCode(DOM.qrcodeContainer, {
+      text: targetUrl,
+      width: 220,
+      height: 220,
+      colorDark: '#090d16',
+      colorLight: '#ffffff',
+      correctLevel: QRCode.CorrectLevel.H
+    });
+  } else {
+    // Highly reliable vector image fallback
+    const qrImg = document.createElement('img');
+    qrImg.src = `https://api.qrserver.com/v1/create-qr-code/?size=220x220&margin=0&data=${encodeURIComponent(targetUrl)}`;
+    qrImg.alt = 'QR Code';
+    qrImg.width = 220;
+    qrImg.height = 220;
+    DOM.qrcodeContainer.appendChild(qrImg);
   }
 }
 
@@ -1095,9 +721,7 @@ function drawQRCode(canvas, urlText) {
  * Open QR Code Modal
  */
 function openQrModal() {
-  if (DOM.qrCanvas) {
-    drawQRCode(DOM.qrCanvas, 'https://cv.mohammed.alotaibi.site/');
-  }
+  generateWebsiteQRCode();
   DOM.qrModal.classList.remove('hidden');
 }
 
@@ -1121,14 +745,26 @@ function handleModalBackdropClick(event) {
  * Download QR Code Image
  */
 function downloadQrImage() {
-  if (!DOM.qrCanvas) return;
-  const link = document.createElement('a');
-  link.download = 'Mohammed-Al-Otaibi-CV-QR.png';
-  link.href = DOM.qrCanvas.toDataURL('image/png');
-  document.body.appendChild(link);
-  link.click();
-  document.body.removeChild(link);
-  showToast(cvData[currentLang]?.labels?.qrDownloaded || 'تم تحميل صورة رمز QR بنجاح!');
+  if (!DOM.qrcodeContainer) return;
+  const canvas = DOM.qrcodeContainer.querySelector('canvas');
+  const img = DOM.qrcodeContainer.querySelector('img');
+
+  let dataUrl = '';
+  if (canvas) {
+    dataUrl = canvas.toDataURL('image/png');
+  } else if (img) {
+    dataUrl = img.src;
+  }
+
+  if (dataUrl) {
+    const link = document.createElement('a');
+    link.download = 'Mohammed-Al-Otaibi-CV-QR.png';
+    link.href = dataUrl;
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+    showToast(cvData[currentLang]?.labels?.qrDownloaded || 'تم تحميل صورة رمز QR بنجاح!');
+  }
 }
 
 /**
